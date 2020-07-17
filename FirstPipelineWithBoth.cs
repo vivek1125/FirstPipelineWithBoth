0c4ffab830183117106bb3e3f4ec72e347d04ae3
vivek1125 <v.raj91220@gmail.com>
2020-07-17 19:55:00 +0530

    Adding new file into

---
+++ FirstPipelineWithBoth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public FirstPipelineWithBoth()
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FirstPipelineWithBoth");
+            Console.Read();
+        }
+    }
+}
+
+
